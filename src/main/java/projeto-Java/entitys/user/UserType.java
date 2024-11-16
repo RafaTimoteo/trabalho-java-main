56903073e43a1132566392175b0a7c3d0baf3a65
Rafael Timoteo <rafaeltimoteolima1202@gmail.com>
2024-11-15 23:42:40 -0300

    primeiro commit

---
+++ src/main/java/projeto-Java/entitys/user/UserType.java
@@ -0,0 +1,7 @@
+package Projeto.java.entitys.user;
+
+// Enum do tipo de usuário
+public enum UserType {
+    COMMON,
+    MERCHANT
+}
---
+++ src/main/java/projeto-Java/mocks/SendNotification/SendNotification.java
@@ -0,0 +1,20 @@
+package Projeto.java.mocks.SendNotification;
+
+import Projeto.java.dtos.requests.RequestSendNotification;
+import Projeto.java.dtos.responses.ResponseSendNotification;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/api/v1/notify")
+@RegisterRestClient(configKey = "mock-api")
+public interface SendNotification {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    ResponseSendNotification sendNotification(RequestSendNotification requestSendNotification);
+}
